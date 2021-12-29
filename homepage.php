f3a5d3b099139cd1d2570330b909c643ac636f38
CHAI WEI NING <weining2001@gmail.com>
2021-12-30 01:01:00 +0800

    pbt2

--- homepage.php
+++ homepage.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
 </head>
+
 <body>
     <h1>Home Page</h1>
     <table cellpadding="2" cellspacing="2">
@@ -33,4 +35,5 @@
     </table>
     <a href="changepassword.php">Change Password</a>
 </body>
+
 </html>
--- reset.php
+++ reset.php
@@ -1,6 +1,5 @@
 <?php
-    $kata = password_hash('abc123',PASSWORD_BCRYPT);
-    $myfile = fopen('pswd.txt','w');
-    fwrite($myfile, $kata);
-    fclose($myfile);
-?>
+$kata = password_hash('abc123', PASSWORD_BCRYPT);
+$myfile = fopen('pswd.txt', 'w');
+fwrite($myfile, $kata);
+fclose($myfile);
